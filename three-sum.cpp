1f9214859157a4992dc176c1bf0ba63742288d80
Ahnaf Shahrear Khan <89327822+ahnafshahrear@users.noreply.github.com>
2023-09-03 09:10:56 +0600

    three-sum updated

--- three-sum.cpp
+++ three-sum.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int> &nums)
     {
-        int n = nums.size();
+        int size = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        for (int i = 0; i < n - 2; i++)
+        for (int i = 0; i < size - 2; i++)
         {
             if (nums[i] > 0) break;
             if (i and nums[i] == nums[i - 1]) continue;
-            int j = i + 1, k = n - 1;
-            while (j < k)
+            int start = i + 1, end = size - 1;
+            while (start < end)
             {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) j++;
-                else if (sum > 0) k--;
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum < 0) start++;
+                else if (sum > 0) end--;
                 else
                 {
-                    ans.push_back({nums[i], nums[j], nums[k]});
-                    while (j < k and nums[j] == nums[j + 1])
+                    ans.push_back({nums[i], nums[start], nums[end]});
+                    while (start < end and nums[start] == nums[start + 1]) 
                     {
-                        j++;
+                        start++;
                     }
-                    while (j < k and nums[k] == nums[k - 1])
+                    while (start < end and nums[end] == nums[end - 1]) 
                     {
-                        k--;
+                        end--;
                     }
-                    j++;k--;
+                    start++; 
+                    end--;
                 }
             }
         }
