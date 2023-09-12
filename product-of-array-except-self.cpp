bce38b6c9accc819c6ae8f6bac68430a82ab1f1b
Ahnaf Shahrear Khan <89327822+ahnafshahrear@users.noreply.github.com>
2023-09-12 23:29:16 +0600

    product-of-array-except-self added

---
+++ product-of-array-except-self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int> &nums) 
+    {
+        vector<int> ans(nums.size(), 1);
+        int prefix = 1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            ans[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+        int postfix = 1;
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            ans[i] = ans[i] * postfix;
+            postfix = postfix * nums[i];
+        }
+        return ans;
+    }
+};
