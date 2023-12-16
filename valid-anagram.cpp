8f4eb7a005eae2887fabdf055554337d51f22104
Ahnaf Shahrear Khan <89327822+ahnafshahrear@users.noreply.github.com>
2023-12-16 08:32:51 +0600

    valid-anagram updated

--- valid-anagram.cpp
+++ valid-anagram.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        if(s.size() != t.size()) return false;
+        if (s.size() != t.size()) return false;
         unordered_map<char,int> sMap, tMap;
-        for(int i = 0; i < s.size(); i++)
+        for (int i = 0; i < s.size(); i++)
         {
             sMap[s[i]]++; tMap[t[i]]++;
         }
-        for(int i = 0; i < sMap.size(); i++)
+        for (int i = 0; i < sMap.size(); i++)
         {
-            if(sMap[i] != tMap[i]) return false;
+            if (sMap[i] != tMap[i]) return false;
         }
         return true;
     }
