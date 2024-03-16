74c07794f162a3acb3756d77b917bcdd5fe6d710
Ahnaf Shahrear Khan <89327822+ahnafshahrear@users.noreply.github.com>
2024-03-17 05:34:03 +0600

    remove-nth-node-from-end-of-list.cpp added

---
+++ remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if (head->next == nullptr)
+        {
+            return nullptr;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (n--)
+        {
+            fast = fast->next;
+        }
+
+        if (fast == nullptr) return head->next;
+
+        while (fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return head;
+    }
+};
